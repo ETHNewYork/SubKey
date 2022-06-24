b79df2a170119d4c6cd1d3d6bbce49e3a64339f9
Alexey Malashkevich <am@agilecode.io>
2022-06-24 19:55:57 -0400

    Add dummy NFT contract

---
+++ contracts/MyNFT.sol
@@ -0,0 +1,46 @@
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract MyNFT is ERC721, Ownable {
+
+  using Strings for uint256;
+
+  string baseURI;
+  string public baseExtension = ".json";
+
+  constructor(string memory _initBaseURI) ERC721("NFT Name", "NFT_SYMBOL") {
+    setBaseURI(_initBaseURI);
+  }
+
+  function safeMint(address to, uint256 tokenId) public onlyOwner {
+    _safeMint(to, tokenId);
+  }
+
+  function _baseURI() internal view override returns (string memory) {
+    return baseURI;
+  }
+
+  function setBaseURI(string memory _newBaseURI) public onlyOwner {
+    baseURI = _newBaseURI;
+  }
+
+  function setBaseExtension(string memory _newBaseExtension) public onlyOwner {
+    baseExtension = _newBaseExtension;
+  }
+
+  function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+    require(
+      _exists(tokenId),
+      "ERC721Metadata: URI query for nonexistent token"
+    );
+
+    string memory currentBaseURI = _baseURI();
+    return bytes(currentBaseURI).length > 0
+    ? string(abi.encodePacked(currentBaseURI, tokenId.toString(), baseExtension))
+    : "";
+  }
+
+}
