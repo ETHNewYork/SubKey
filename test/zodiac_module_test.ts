e9dffc22cafcd5b0e42cbb68165bd9a956290a00
Yury Litvinov <litvinov.yury@gmail.com>
2022-06-25 20:25:28 -0400

    test zodiac - placeholder

--- test/zodiac_module_test.ts
+++ test/zodiac_module_test.ts
@@ -1,8 +1,14 @@
 import {expect} from "chai";
 import {ethers} from "hardhat";
+import {TestAvatar} from "../typechain";
 
 describe("Zodiac integration with Permissioned wallet", function () {
-  it("", async function () {
+  it("should emit event because of successful set up", async () => {
+    const avatarFactory = await ethers.getContractFactory("TestAvatar");
+    const avatarContract: TestAvatar = await avatarFactory.deploy();
+
+    const nftFactory = await ethers.getContractFactory("TestNFT");
+    const nftContract = await nftFactory.deploy();
 
   });
 });
