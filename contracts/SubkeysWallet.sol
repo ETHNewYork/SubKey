f3e9df7dd7a14f44553f6723ad55328f4fb968fa
Yury Litvinov <litvinov.yury@gmail.com>
2022-06-25 23:53:39 -0400

    refactoring

--- contracts/SubkeysWallet.sol
+++ contracts/SubkeysWallet.sol
@@ -21,55 +21,25 @@ contract SubkeysWallet is Ownable {
     bytes predicateParams;
   }
 
-  function checkSignatureValid(address signer, bytes32 hash, bytes memory signature) internal pure {
-    bytes32 etherHash = getHashEthereum(bytes32ToString(hash));
-    (address recovered, ECDSA.RecoverError error) = ECDSA.tryRecover(etherHash, signature);
-    require(signer == recovered, "Invalid signature");
-  }
-
-  function getPermissionHash(Permission memory permission) public pure returns (bytes32){
-    return keccak256(
-      abi.encode(
-        permission.caller
-      )
-    );
-  }
-
-  function getHashEthereum(string memory hash) public pure returns (bytes32){
-    return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hash));
-  }
-
-  function bytes32ToString(bytes32 _bytes32) public pure returns (string memory) {
-    uint8 i = 0;
-    while (i < 32 && _bytes32[i] != 0) {
-      i++;
-    }
-    bytes memory bytesArray = new bytes(i);
-    for (i = 0; i < 32 && _bytes32[i] != 0; i++) {
-      bytesArray[i] = _bytes32[i];
-    }
-    return string(bytesArray);
-  }
-
-
   function execute(Call memory call,
     Permission memory permission, // created by owner
     bytes memory permissionSignature // created by owner
   ) public {
-    //1. Check that Permission was signed by the owner => we trust the Permission data
-    checkSignatureValid(owner(), getPermissionHash(permission), permissionSignature);
+    if (msg.sender != owner()) {
+      //1. Check that Permission was signed by the owner => we trust the Permission data
+      checkSignatureValid(owner(), getPermissionHash(permission), permissionSignature);
 
-    //2. Check that caller is the same as approved by the Permission
-    require(permission.caller == msg.sender, "Wrong transaction sender");
+      //2. Check that caller is the same as approved by the Permission
+      require(permission.caller == msg.sender, "Wrong transaction sender");
 
-    //3. Check that Predicate(transaction)=true => we trust transaction
-    permission.predicate.check(call, permission.predicateParams);
-
+      //3. Check that Predicate(transaction)=true => we trust transaction
+      permission.predicate.check(call, permission.predicateParams);
+    }
     //4. Execute trusted transaction
-    _execute(call);
+    executeTransaction(call);
   }
 
-  function _execute(Call memory call) private returns (bytes memory) {
+  function executeTransaction(Call memory call) private returns (bytes memory) {
     (bool success, bytes memory result) = call.to.call(call.data);
     if (!success) {
       if (result.length < 68) revert();
@@ -81,4 +51,33 @@ contract SubkeysWallet is Ownable {
     return result;
   }
 
+  function getPermissionHash(Permission memory permission) public pure returns (bytes32){
+    return keccak256(
+      abi.encode(
+        permission.caller
+      )
+    );
+  }
+
+  function checkSignatureValid(address signer, bytes32 hash, bytes memory signature) private pure {
+    bytes32 etherHash = getHashEthereum(bytes32ToString(hash));
+    (address recovered, ECDSA.RecoverError error) = ECDSA.tryRecover(etherHash, signature);
+    require(signer == recovered, "Invalid signature");
+  }
+
+  function getHashEthereum(string memory hash) private pure returns (bytes32){
+    return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hash));
+  }
+
+  function bytes32ToString(bytes32 _bytes32) private pure returns (string memory) {
+    uint8 i = 0;
+    while (i < 32 && _bytes32[i] != 0) {
+      i++;
+    }
+    bytes memory bytesArray = new bytes(i);
+    for (i = 0; i < 32 && _bytes32[i] != 0; i++) {
+      bytesArray[i] = _bytes32[i];
+    }
+    return string(bytesArray);
+  }
 }
