83c1d672b1aa63dc07da7cd70271bceb8b781bed
Alexey Malashkevich <am@agilecode.io>
2022-06-26 01:49:59 -0400

    Refactoring

--- test/zodiac_module_test.ts
+++ test/zodiac_module_test.ts
@@ -1,5 +1,5 @@
-import { expect } from "chai";
-import { ethers } from "hardhat";
+import {expect} from "chai";
+import {ethers} from "hardhat";
 import {
   PredicateImplV1,
   SubkeysWallet,
@@ -7,36 +7,36 @@ import {
   TestNFT,
   ZeroGasRoleModule,
 } from "../typechain";
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import {SignerWithAddress} from "@nomiclabs/hardhat-ethers/signers";
+import {deployContract} from "./common";
 
 describe("Zodiac integration with Permissioned wallet", function () {
   let walletOwner: SignerWithAddress,
-      nftReceiver: SignerWithAddress,
-      thirdParty: SignerWithAddress,
-      wrongThirdParty: SignerWithAddress;
+    nftReceiver: SignerWithAddress,
+    thirdParty: SignerWithAddress,
+    wrongThirdParty: SignerWithAddress;
 
   let predicateContract: PredicateImplV1;
   let nftContract: TestNFT;
+  let avatarContract: TestAvatar;
 
   beforeEach(async () => {
     [walletOwner, thirdParty, nftReceiver, wrongThirdParty] =
-        await ethers.getSigners();
+      await ethers.getSigners();
 
     // deploy predicate (one per chain)
-    predicateContract = await deployPredicate(walletOwner);
+    predicateContract = await deployContract(walletOwner, "PredicateImplV1");
 
     // create avatar
-    const avatarFactory = await ethers.getContractFactory("TestAvatar");
-    const avatarContract: TestAvatar = await avatarFactory.deploy();
+    avatarContract = await deployContract(walletOwner, "TestAvatar");
 
     // create NFT collection and transfer to avatar
-    nftContract = await deployNftContract(walletOwner);
+    nftContract = await deployContract(walletOwner, "TestNFT");
     nftContract.connect(walletOwner).transferOwnership(avatarContract.address);
   });
 
   it("should emit event because of successful set up", async () => {
     const [walletOwner, thirdParty, nftReceiver] = await ethers.getSigners();
-
 
     // 1. create permission for thirdParty to mint NFT (off chain)
     const permissionToMint = createPermissionToMint(thirdParty.address);
@@ -54,27 +54,6 @@ describe("Zodiac integration with Permissioned wallet", function () {
     // };
   });
 
-  async function deployPredicate(signer: SignerWithAddress) {
-    const predicateFactory = await ethers.getContractFactory("PredicateImplV1");
-    const predicateContract = await predicateFactory.connect(signer).deploy();
-    await predicateContract.deployed();
-    return predicateContract;
-  }
-
-  async function deployNftContract(signer: SignerWithAddress) {
-    const nftFactory = await ethers.getContractFactory("TestNFT");
-    const nftContract = await nftFactory.connect(signer).deploy();
-    await nftContract.deployed();
-    return nftContract;
-  }
-
-  async function deployWallet(signer: SignerWithAddress) {
-    const walletFactory = await ethers.getContractFactory("SubkeysWallet");
-    const walletContract = await walletFactory.connect(signer).deploy();
-    await walletContract.deployed();
-    return walletContract;
-  }
-
   function createPermissionToMint(address: string) {
     const iface = new ethers.utils.Interface([
       "function safeMint(address to, uint256 tokenId)",
@@ -84,8 +63,8 @@ describe("Zodiac integration with Permissioned wallet", function () {
       predicate: predicateContract.address,
       caller: address,
       predicateParams: ethers.utils.defaultAbiCoder.encode(
-          ["address", "bytes4"],
-          [nftContract.address, iface.getSighash("safeMint")]
+        ["address", "bytes4"],
+        [nftContract.address, iface.getSighash("safeMint")]
       ),
     };
   }
